fe2ea07fe52e3923e961977b80702497b5f4aa11
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-05 14:07:31 +0500

    Added model

---
+++ N67.EduCourse.Domin/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace N67.EduCourse.Domin.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; } = default!;
+
+    public string LastName { get; set; } = default!;
+
+    public virtual ICollection<Course> TeacherCourses { get; set; }
+
+    public virtual ICollection<CourseStudent> CourseStudents { get; set; }
+}
