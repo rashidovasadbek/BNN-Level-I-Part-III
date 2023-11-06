1a3cc29045a1b672c4da8aba82baafa8f72072f2
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-06 21:36:05 +0500

    Added HostConfiguration

---
+++ N67.EduCourse.Api/Configuration/HostConfigurationExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using N67.EduCourse.Application.Common.Identity.Services;
+using N67.EduCourse.Infrastructure.Common.Identity.Services;
+using Persistance.DataContext;
+
+namespace N67.EduCourse.Api.Configuration;
+
+public static partial class HostConfiguration
+{
+    private static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDBContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddIdentityInfrastructure(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IUserService, UserService>();
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddEndpointsApiExplorer();
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+
+        return builder;
+    }
+
+    private static WebApplication UseDevTools(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+}
