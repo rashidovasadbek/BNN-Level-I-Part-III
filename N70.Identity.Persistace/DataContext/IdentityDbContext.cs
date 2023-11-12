f942a973769a1ce5d6f40ea863a46547faeddeab
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-12 15:43:55 +0500

    Added part of Clean Architecture

---
+++ N70.Identity.Persistace/DataContext/IdentityDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using N70.Identity.Domin.Entities;
+
+namespace N70.Identity.Persistace.DataContext;
+
+public class IdentityDbContext : DbContext
+{
+    //public DbSet<User> Users => Set<User>();
+
+    public DbSet<Role> Roles => Set<Role>();
+
+    public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IdentityDbContext).Assembly);
+    }
+}
