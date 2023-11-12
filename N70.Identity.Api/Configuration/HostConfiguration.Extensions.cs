c37b4876fc7c86981f816a68c8777347f02c5981
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-12 15:39:27 +0500

    hostConfiguration added

---
+++ N70.Identity.Api/Configuration/HostConfiguration.Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using N70.Identity.Persistace.DataContext;
+
+namespace N70.Identity.Api.Configuration;
+
+public static partial class HostConfiguration
+{
+    private static WebApplicationBuilder AddPersistance(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<IdentityDbContext>(optoins =>
+            optoins.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        return builder;
+    }
+}
---
+++ N70.Identity.Api/Configuration/HostConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace N70.Identity.Api.Configuration;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder.AddPersistance();
+
+        return new(builder);
+    }
+}
