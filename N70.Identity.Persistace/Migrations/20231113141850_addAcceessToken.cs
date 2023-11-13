df58c8c2e4c56e2a222e04909d7b52532c9a198c
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-13 22:07:36 +0500

    Added AccessToken Configuration

---
+++ N70.Identity.Persistace/Migrations/20231113141850_addAcceessToken.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace N70.Identity.Persistace.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAcceessToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccessTokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false),
+                    IsRevoked = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccessTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AccessTokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("6d3503ab-1a35-47b9-be09-b24ff4fbf6bf"),
+                column: "CreatedTime",
+                value: new DateTime(2023, 11, 13, 14, 18, 50, 409, DateTimeKind.Utc).AddTicks(3179));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("7d07ea1f-9be7-48f0-ad91-5b83a5806baf"),
+                column: "CreatedTime",
+                value: new DateTime(2023, 11, 13, 14, 18, 50, 409, DateTimeKind.Utc).AddTicks(3182));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("df290f92-dd78-4fa7-9ce3-6b0056a8b68f"),
+                column: "CreatedTime",
+                value: new DateTime(2023, 11, 13, 14, 18, 50, 409, DateTimeKind.Utc).AddTicks(3183));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccessTokens_UserId",
+                table: "AccessTokens",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccessTokens");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("6d3503ab-1a35-47b9-be09-b24ff4fbf6bf"),
+                column: "CreatedTime",
+                value: new DateTime(2023, 11, 12, 10, 30, 33, 578, DateTimeKind.Utc).AddTicks(3034));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("7d07ea1f-9be7-48f0-ad91-5b83a5806baf"),
+                column: "CreatedTime",
+                value: new DateTime(2023, 11, 12, 10, 30, 33, 578, DateTimeKind.Utc).AddTicks(3036));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("df290f92-dd78-4fa7-9ce3-6b0056a8b68f"),
+                column: "CreatedTime",
+                value: new DateTime(2023, 11, 12, 10, 30, 33, 578, DateTimeKind.Utc).AddTicks(3038));
+        }
+    }
+}
