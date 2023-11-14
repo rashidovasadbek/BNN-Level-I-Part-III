3c03b9c7e8cbbdb416d61045ad707fd940b6020f
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-14 22:08:01 +0500

    Added DbContext

---
+++ N73.Notifications.Persistance/DataContexts/NotificationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using N73.Notifications.Domin.Entities;
+
+namespace N73.Notifications.Persistance.DataContexts;
+
+public class NotificationDbContext : DbContext
+{
+
+    //public DbSet<SmsTemplate> SmsTemplates => Set<SmsTemplate>();
+
+    //public DbSet<SmsHistory> SmsHistories => Set<SmsHistory>();
+
+    public DbSet<EmailTemplate> EmailTemplates => Set<EmailTemplate>();
+
+    public DbSet<EmailHistory> EmailHistories => Set<EmailHistory>();
+    
+    //public DbSet<User> Users => Set<User>();
+    
+    public NotificationDbContext(DbContextOptions<NotificationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(NotificationDbContext).Assembly);
+    }
+    
+}
