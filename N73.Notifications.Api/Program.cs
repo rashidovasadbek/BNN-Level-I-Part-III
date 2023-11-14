19054ebe1a72484f95e227a1ef4d6b9c20cfb2ad
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-14 22:09:22 +0500

    Added Api Configurstion

--- N73.Notifications.Api/Program.cs
+++ N73.Notifications.Api/Program.cs
@@ -1,6 +1,9 @@
+using Notifications.Api;
+
 var builder = WebApplication.CreateBuilder(args);
+
+await builder.ConfigureAsync();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
-app.Run();
+await app.RunAsync();
