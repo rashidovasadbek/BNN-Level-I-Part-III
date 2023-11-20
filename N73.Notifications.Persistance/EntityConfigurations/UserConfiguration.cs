9634824d35334e02526611977a415ffe225c74f9
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-20 14:15:41 +0500

    Added UserConfiguration and UserSettingsConfiguration in EntityConfiguration

---
+++ N73.Notifications.Persistance/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using N73.Notifications.Domin.Entities;
+
+namespace N73.Notifications.Persistance.EntityConfigurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+    }
+}
---
+++ N73.Notifications.Persistance/EntityConfigurations/UserSettingsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using N73.Notifications.Domin.Entities;
+
+namespace N73.Notifications.Persistance.EntityConfigurations;
+
+public class UserSettingsConfiguration : IEntityTypeConfiguration<UserSettings>
+{
+    public void Configure(EntityTypeBuilder<UserSettings> builder)
+    {
+        builder.HasOne<User>().WithOne(user => user.UserSettings).HasForeignKey<UserSettings>(settings => settings.Id);
+    }
+}
