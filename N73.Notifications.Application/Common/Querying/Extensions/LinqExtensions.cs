8d4dd9e808525e7ca94d0883ea9dbfff2f6eb06a
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-20 22:34:01 +0500

    Added Extension method

---
+++ N73.Notifications.Application/Common/Querying/Extensions/LinqExtensions.cs
@@ -0,0 +1,20 @@
+﻿using N73.Notifications.Application.Common.Models.Quering;
+
+namespace N73.Notifications.Application.Common.Querying.Extensions;
+
+public static class LinqExtensions
+{
+    public static IQueryable<TSourse> ApplyPagination<TSourse>(this IQueryable<TSourse> sourse,
+        FilterPagination filterPagination)
+    {
+        return sourse.Skip((int)((filterPagination.PageToken - 1) * filterPagination.PageSize))
+            .Take((int)filterPagination.PageSize);
+    }
+
+    public static IEnumerable<TSourse> ApplyPagination<TSourse>(this IEnumerable<TSourse> sourse,
+        FilterPagination filterPagination)
+    {
+        return sourse.Skip((int)((filterPagination.PageToken - 1) * filterPagination.PageSize))
+            .Take((int)filterPagination.PageSize);
+    }
+}
