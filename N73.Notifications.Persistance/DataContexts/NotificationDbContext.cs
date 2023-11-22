997f6769e2afa828e0fd7c5b20b9f4e7ea8cb4e7
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-22 22:19:43 +0500

    Update NotificationDbContext

--- N73.Notifications.Persistance/DataContexts/NotificationDbContext.cs
+++ N73.Notifications.Persistance/DataContexts/NotificationDbContext.cs
@@ -9,11 +9,14 @@ public class NotificationDbContext : DbContext
     public DbSet<SmsTemplate> SmsTemplates => Set<SmsTemplate>();
 
     public DbSet<SmsHistory> SmsHistories => Set<SmsHistory>();
+    
     public DbSet<EmailTemplate> EmailTemplates => Set<EmailTemplate>();
 
     public DbSet<EmailHistory> EmailHistories => Set<EmailHistory>();
     
-    //public DbSet<User> Users => Set<User>();
+    public DbSet<User> Users => Set<User>();
+
+    public DbSet<UserSettings> UserSettings => Set<UserSettings>();
     
     public NotificationDbContext(DbContextOptions<NotificationDbContext> options) : base(options)
     {
