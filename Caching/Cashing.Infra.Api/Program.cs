cbeba6974a3c96ad78e98b2a6ac891b241f33134
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-04 15:19:37 +0500

    Added Clean Architecture

---
+++ Caching/Cashing.Infra.Api/Program.cs
@@ -0,0 +1,11 @@
+using Cashing.Infra.Api.Configurations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+await builder.ConfigureAsync();
+
+var app = builder.Build();
+
+await app.ConfigureAsync();
+
+await app.RunAsync();
---
+++ Caching/Cashing.Infra.Domain/Common/Entities/IEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Cashing.Infra.Domain.Common.Entities;
+
+public interface IEntity
+{
+    Guid Id { get; set; }
+}
