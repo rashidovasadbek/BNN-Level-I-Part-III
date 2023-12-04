ea2f8c8457327db1632d118d39c030b04992632d
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-04 15:19:58 +0500

    AddConfigurations

---
+++ Caching/Cashing.Infra.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Cashing.Infra.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+
+        return builder;
+    }
+
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+   
+}
---
+++ Caching/Cashing.Infra.Api/Configurations/HostConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Cashing.Infra.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder.AddExposers();
+
+        return new(builder);
+    }
+
+    public static async ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        app.UseExposers();
+
+        return (app);
+    }
+}
