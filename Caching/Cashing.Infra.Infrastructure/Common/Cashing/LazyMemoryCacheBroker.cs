c556812913370d6917c77c4b8127265d8d097719
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-05 22:45:58 +0500

    Added Broker

---
+++ Caching/Cashing.Infra.Infrastructure/Common/Cashing/LazyMemoryCacheBroker.cs
@@ -0,0 +1,48 @@
+﻿using Cashing.Infra.Infrastructure.Common.Settings;
+using Cashing.Infra.Persistence.cashing;
+using LazyCache;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+
+namespace Cashing.Infra.Infrastructure.Common.Cashing;
+
+public class LazyMemoryCacheBroker(IAppCache appCache, IOptions<CacheSettings> cacheSettings) : ICacheBroker
+{
+
+    private readonly CacheSettings _cacheSettings = cacheSettings.Value;
+
+    public async ValueTask<T> GetAsync<T>(string key) => await appCache.GetAsync<T>(key);
+
+    public async ValueTask<T> GetOrSetAsync<T>(string key, Func<Task<T>> valueFactory)
+    {
+        var options = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(_cacheSettings.AbsoluteExpirationInMinutes),
+            SlidingExpiration = TimeSpan.FromMinutes(_cacheSettings.SlidingExpirationInMinutes)
+        };
+
+        return await appCache.GetOrAddAsync(key, valueFactory, options);
+    }
+
+    public ValueTask SetASync<T>(string key, T value)
+    {
+        var options = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes((_cacheSettings.AbsoluteExpirationInMinutes)),
+            SlidingExpiration = TimeSpan.FromMinutes(_cacheSettings.SlidingExpirationInMinutes)
+        };
+        
+        appCache.Add(key, value, options);
+        
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask DeleteAsync(string key)
+    {
+        appCache.Remove(key);
+        
+        return ValueTask.CompletedTask;
+    }
+    
+    //public MemoryCacheEntryOptions GetCacheEntryOptions(CacheO)
+}
---
+++ Caching/Cashing.Infra.Infrastructure/Common/Settings/CacheSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Cashing.Infra.Infrastructure.Common.Settings;
+
+public class CacheSettings
+{
+    public int AbsoluteExpirationInMinutes { get; set; }
+    
+    public int SlidingExpirationInMinutes { get; set; }
+}
