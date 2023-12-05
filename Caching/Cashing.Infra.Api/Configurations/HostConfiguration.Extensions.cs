fbfc562b3c95fe8f92227983f3f31a3dc186df24
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-05 22:46:18 +0500

    Add HostConfiguration

--- Caching/Cashing.Infra.Api/Configurations/HostConfiguration.Extensions.cs
+++ Caching/Cashing.Infra.Api/Configurations/HostConfiguration.Extensions.cs
@@ -1,28 +1,70 @@
-﻿using Cashing.Infra.Persistence.DataContexts;
+﻿using Cashing.Infra.Api.Data;
+using Cashing.Infra.Application.Common.Identity.Service;
+using Cashing.Infra.Infrastructure.Common.Cashing;
+using Cashing.Infra.Infrastructure.Common.Identity.Services;
+using Cashing.Infra.Infrastructure.Common.Settings;
+using Cashing.Infra.Persistence.cashing;
+using Cashing.Infra.Persistence.DataContexts;
+using Cashing.Infra.Persistence.Repositories;
+using Cashing.Infra.Persistence.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cashing.Infra.Api.Configurations;
 
 public static partial class HostConfiguration
 {
+    private static WebApplicationBuilder AddCaching(this WebApplicationBuilder builder)
+    {
+        //register cache settings
+        builder.Services.Configure<CacheSettings>(builder.Configuration.GetSection(nameof(CacheSettings)));
+        
+        //register lazy memory cache
+        builder.Services.AddLazyCache();
+        builder.Services.AddSingleton<ICacheBroker, LazyMemoryCacheBroker>();
 
+        return builder;
+    }
+    
     private static WebApplicationBuilder AddIdentityInfrastructure(this WebApplicationBuilder builder)
     {
         builder.Services.AddDbContext<IdentityDbContext>(
             options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+        
+        // register repositories
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        
+        //register services
+        builder.Services.AddScoped<IUserService, UserService>();
 
         return builder;
     }
+
+   
     private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
     {
         builder.Services.AddRouting(options => options.LowercaseUrls = true);
         builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen();
 
         return builder;
     }
+    
+    private static async ValueTask<WebApplication> SeedDataAsync(this WebApplication app)
+    {
+        var serviceScope = app.Services.CreateScope();
+        await serviceScope.ServiceProvider.InitializeSeedAsync();
 
+        return app;
+    }
+    
     private static WebApplication UseExposers(this WebApplication app)
     {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        
         app.MapControllers();
 
         return app;
--- Caching/Cashing.Infra.Api/Configurations/HostConfiguration.cs
+++ Caching/Cashing.Infra.Api/Configurations/HostConfiguration.cs
@@ -4,13 +4,14 @@ public static partial class HostConfiguration
 {
     public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
     {
-        builder.AddExposers().AddIdentityInfrastructure();
+        builder.AddCaching().AddExposers().AddIdentityInfrastructure();
 
         return new(builder);
     }
 
     public static async ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
     {
+        await app.SeedDataAsync();
         app.UseExposers();
 
         return (app);
